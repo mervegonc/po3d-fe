9891dd8351dc6ae175d77c34364b65b8dcd516d2
merve-goncu <merve.gnc.946@gmail.com>
2024-11-26 16:36:56 +0300

    creation of frontend pages

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // Ana renk
+    },
+    secondary: {
+      main: '#ff9800', // İkincil renk
+    },
+  },
+});
+
+export default theme;
